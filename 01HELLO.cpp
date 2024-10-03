d6e2d5f78d8769c2f8056dea980693acd8bf25da
012424miko <capillanmiko06@gmail.com>
2024-10-03 08:36:58 +0800

    Add files via upload

--- 01HELLO.cpp
+++ 01HELLO.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
-int main(){
-	std :: cout << "Hello WOrld";
+
+using namespace std;
+int main()
+{
+	cout << "Our Lady Of Lourdes College Foundation" <<endl;
+	cout << "COMPUTER OF SCIENCE STUDIES" <<endl;
+	cout << "BSIT AND FIRST YEAR" <<endl;
+	cout << "Miko B Capillan" <<endl;
+	cout << "BRGY SANLORENZO SANTA ELENE CAM.NORTE" <<endl;
+	
 	return 0;
 }
