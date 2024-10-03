0f9d54ec0e2665c77ec03a1dba672093fbaea860
MIKO <capillanmiko06@gmail.com>
2024-10-03 10:17:04 +0800

    Update 01HELLO.cpp

--- 01HELLO.cpp
+++ 01HELLO.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
 	cout << "Our Lady Of Lourdes College Foundation" <<endl;
-	cout << "COMPUTER OF SCIENCE STUDIES" <<endl;
+	cout << "COLLEGE OF SCIENCE STUDIES" <<endl;
 	cout << "BSIT AND FIRST YEAR" <<endl;
 	cout << "Miko B Capillan" <<endl;
 	cout << "BRGY SANLORENZO SANTA ELENE CAM.NORTE" <<endl;
