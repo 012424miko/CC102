e3cd9d7649fe629f9fe3247107bd7d128a3655bf
MIKO <capillanmiko06@gmail.com>
2024-10-03 11:30:01 +0800

    Add files via upload

---
+++ 04Basicinput.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+int main ()
+{
+	float name, course, school;
+	
+	cout << "Nickname: ";
+	cin >> name;
+	cout << "Course-Year Level: ";
+	cin >> course; 
+	cout << "Scool:";
+	cin >> school;
+	
+	cout << "Wow Congrats" << name << "!" << course << " Which is one of the Center of Excellence in Tertiary Education";
+	return 0; 
+}
+
+	
+	
